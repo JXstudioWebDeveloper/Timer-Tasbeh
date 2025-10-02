e8ca1a64871e6b3743e37e1324c449e8009e3cd3
Jamshidbek Oydinov <jamshidoydinov929@gmail.com>
2025-10-02 21:54:32 +0500

    add

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const sum = document.getElementById('count');
 const plusBtn = document.getElementById('plus');
 const deletBtn = document.getElementById('delet');
-let s = 0;
+let s = localStorage.getItem('counter')
+  ? parseInt(localStorage.getItem('counter'))
+  : 0;
+sum.innerHTML = s;
 plusBtn.addEventListener('click', () => {
   s++;
   sum.innerHTML = s;
+  localStorage.setItem('counter', s);
 });
 deletBtn.addEventListener('click', () => {
   s = 0;
   sum.innerHTML = s;
+  localStorage.removeItem('counter');
 });
 const soat = document.getElementById('soat');
 const daqiqa = document.getElementById('daqiqa');
 const sekund = document.getElementById('sekund');
 function showClock() {
   let now = new Date();
-  let hour = now.getHours();
-  let minut = now.getMinutes();
-  let second = now.getSeconds();
-  hour = hour.toString().padStart(2, '0');
-  minut = minut.toString().padStart(2, '0');
-  second = second.toString().padStart(2, '0');
+  let hour = now.getHours().toString().padStart(2, '0');
+  let minut = now.getMinutes().toString().padStart(2, '0');
+  let second = now.getSeconds().toString().padStart(2, '0');
   soat.innerHTML = hour;
   daqiqa.innerHTML = minut;
   sekund.innerHTML = second;
